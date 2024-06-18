ae7719921b71c5eea136391865864db6db690238
Alexia Falcone <alexiafalcone1995@gmail.com>
2024-06-17 21:47:35 -0300

    mostrar prod

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -10,20 +10,8 @@ routerViews.get('/products', async (req, res) => {
       limit = parseInt(limit);
       page = parseInt(page);
       let result = await manager.getProductsPaginate(page, category, sort);
-      let { docs, totalDocs, totalPages, hasNextPage, hasPrevPage, prevPage, nextPage } = result;
-      const response = {
-         payload: docs,
-         totalPages,
-         hasNextPage,
-         hasPrevPage,
-         page,
-         prevPage,
-         nextPage,
-         totalDocs,
-         prevLink: page > 1 ? `/home?limit=${limit}&page=${page - 1}&sort=${sort || ''}&query=${category || ''}` : null,
-         nextLink: page < totalPages ? `/home?limit=${limit}&page=${page + 1}&sort=${sort || ''}&query=${category || ''}` : null  
-      }
-      return res.render('home', response)
+      console.log(result)
+      return res.render('home', result)
    } catch (error) {
       res.status(500).json({ msg: 'No se encontraron productos' })
    }
