c3d9dee4e8a7d87cfe8d9c27106d5e25270f4a8e
alexiafalcone <alexiafalcone1995@gmail.com>
2024-07-15 19:39:33 -0300

    .env OK

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const portConection = process.env.PORT
-console.log(portConection);
 
 export const mongoConection = process.env.MONGO_URL;
-console.log(mongoConection);
+
